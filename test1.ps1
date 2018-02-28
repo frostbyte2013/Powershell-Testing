6fb8035a52a0eb1709e13a2328cca8714c3eed61
frostbyte2013 <nickandrew.lyp@gmail.com>
2018-02-27 21:11:45 -0500

    working with switch

--- test1.ps1
+++ test1.ps1
@@ -1,6 +1,6 @@
 
 
-#$Word2 = 'on'
+$results = read-host -Prompt ('Select an Option:')
 $Computer = ("13.82.174.235", "13.82.174.235")
  
 $cred = Get-Credential -message "enter password" -UserName "SteamCMD\nick"
@@ -8,15 +8,21 @@ $sitename = read-host -Prompt ('Enter Site Name:')
 # Using Scope Modifier
 # (1st Option -- works in PowerShell 3.0 and up)
 
+switch($results)
+{
+    0{
+        foreach ($comp in $Computer)
+        {
 
-foreach ($comp in $Computer){
+        Invoke-Command -ConnectionUri https://13.82.174.235:5986 -Credential $cred -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck) -Authentication Negotiate -ScriptBlock {
+        #Write-Verbose -Message "$Using:Word1 $Using:Word2 $env:COMPUTERNAME (Using Scope Modifer)" -Verbose
+        #get-windowsfeature -name Net-wcf* | ft Name, InstallState
 
-    Invoke-Command -ConnectionUri https://13.82.174.235:5986 -Credential $cred -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck) -Authentication Negotiate -ScriptBlock {
-    #Write-Verbose -Message "$Using:Word1 $Using:Word2 $env:COMPUTERNAME (Using Scope Modifer)" -Verbose
-    #get-windowsfeature -name Net-wcf* | ft Name, InstallState
+        New-IISSite -physicalpath "C:\inetpub\test" -BindingInformation "*:80:*" -name $Using:sitename
+            }
+        }
 
-    New-IISSite -physicalpath "C:\inetpub\test" -BindingInformation "*:80:*" -name $Using:sitename
+     }
 
-}
-
+    1{"Test"}
 }
